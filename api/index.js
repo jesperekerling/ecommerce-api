7d2d2dc1bf9c281efb5874196bf0303e3586d960
Jesper Ekerling <jesper@forme.se>
2024-04-11 01:27:50 +0200

    Url update

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://ecommerce.ekerling.com'
+  origin: ['https://ecommerce.ekerling.com', 'http://localhost']
 }));
 
 const db = require("./../db-config")
