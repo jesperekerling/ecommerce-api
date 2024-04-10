b0f9c84b6c7336633294cd2b5a492336698c52a0
Jesper Ekerling <jesper@forme.se>
2024-04-10 13:58:11 +0200

    Tailwind fix

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.js',
+    // Add paths to all of the templates and components in your project
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
