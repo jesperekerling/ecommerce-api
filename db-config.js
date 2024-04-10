003d2501695a0b39a509a1de9743a39d74f82e6b
Jesper Ekerling <jesper@forme.se>
2024-04-10 16:31:16 +0200

    Error handeling on mongoose connect added

--- db-config.js
+++ db-config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).catch(error => console.error('Error connecting to MongoDB', error));
